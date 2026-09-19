ee7a3cc472046d175d3e87abe82e14de969f38b8
utkarsh9368 <utkarsh.25b01010190@abes.ac.in>
2026-09-19 23:10:07 +0530

    Add root build.sh for Render build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+cd backend
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py migrate
